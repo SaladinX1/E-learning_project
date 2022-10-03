c546cc08b150d124b2ff832137a8903977db0970
Saladin <hopefulhorizon01@gmail.com>
2022-10-03 20:46:26 +0200

    Etablissement du serveur

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+// const path = require("path");
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(express.json());
+
+
+module.exports = app;
---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const app = require('./app');
+const http = require('http');
+const server = http.createServer(app);
+const port = 3000;
+
+server.listen( port || process.env.PORT, () => {
+    console.log(`Server is listening on http://localhost/${port || process.env.PORT}`)}) 
+
+
+
+    app.get('/health', function (req, res) {
+        res.send("The API is working properly congratulations !")
+    })
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
