8c9717369166ddc6fcb664b1e23c94fd3dedaf92
Saladin <hopefulhorizon01@gmail.com>
2022-10-06 09:34:19 +0200

    intégration de la database dans le fichier app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-// const path = require("path");
+const path = require("path");
 const cors = require("cors");
+const db = require('./Database/db.script');
+const modelUser = require('./Models/User');
+
+app.use(modelUser);
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json()); 
+
 
 
 module.exports = app;
