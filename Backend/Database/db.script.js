5e3197ec09906fb55a384e1015bf45368a5382b0
Saladin <hopefulhorizon01@gmail.com>
2022-10-06 09:34:59 +0200

    Etablissement de la connexion à la base de données

--- Backend/Database/db.script.js
+++ Backend/Database/db.script.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2');
 
-    const db = mysql.createConnection( 
-        host="localhost",
-        user="Saladin_Rising",
-        password='Razorback2.2',
-        database='normesse_formations'
-     )
+    const db = mysql.createConnection({   host: "localhost",   user: "Saladin_Rising",   password: "Razorback2.2" });
 
-        db.connect();
+         db.connect(function(err) {   if (err) throw err;   console.log("Connecté à la base de données MySQL!"); });
+
+
+          db.query('USE normesse_formations', function (err, result) { if (err) throw err; console.log(result);})
+          db.query('SHOW TABLES', function (err, result) { if (err) throw err; console.log(result);})
+          db.query('SHOW COLUMNS FROM user', function (err, result) { if (err) throw err; console.log(result);})
+         
 
     module.exports = db;
 
+    
+    
+    
