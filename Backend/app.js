d0a24d1398b52396a7cb89392b586f93fca35b20
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-10-27 17:45:52 +0200

    Installation de body-parser et de bootstrap

--- Backend/app.js
+++ Backend/app.js
@@ -5,20 +5,19 @@ const cors = require("cors");
 const db = require('./Database/db.script');
 const userRoutes = require('./Routes/User');
 const auth = require('./Midlewares/auth');
-const bodyParser = require('body-parser');
+ const bodyParser = require('body-parser');
 
-app.use(cors());
+ app.use(cors());
 
 //  app.use(express.json()); 
 //  app.use(express.urlencoded({
 //     extended: true
 //   })); 
 
-app.use(bodyParser.json())
-
 app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  extended: true
+}));
+app.use(bodyParser.json())
 
 app.use('/api', userRoutes);
 
