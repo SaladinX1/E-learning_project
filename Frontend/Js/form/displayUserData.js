d3dad5c221ddb72fb4b087ec90c15a1ab26ca0a4
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-11-02 17:23:01 +0100

    Modification du placement de la promesse .then() dans le displayUserData.js

--- Frontend/Js/form/displayUserData.js
+++ Frontend/Js/form/displayUserData.js
@@ -14,17 +14,17 @@ fetch(url , {
     },
 })
 .then( data => {
-   return data.json();
-})
-.then( res => {
+    data.json()
+    .then( res => {
+        console.log(res);
 
-    profilInfo.innerHTML += `<h3>Nom : ${res.name} </h3>
-                            <h3>Prénom : ${res.secondName}</h3>
-                            <h3>email : ${res.email}</h3>
-                            <h3>Tél : ${res.telephone}</h3>
-                            <h3>Mot de passe : ${res.password}</h3>
-                            <h3>Document type : ${res.documentType}</h3> 
-                            `
-
+        profilInfo.innerHTML += `<h3>Nom : ${res.name} </h3>
+                                <h3>Prénom : ${res.secondName}</h3>
+                                <h3>email : ${res.email}</h3>
+                                <h3>Tél : ${res.telephone}</h3>
+                                <h3>Mot de passe : ${res.password}</h3>
+                                <h3>Document type : ${res.documentType}</h3> 
+                                `
+    })
 })
 .catch( err => { throw err })
