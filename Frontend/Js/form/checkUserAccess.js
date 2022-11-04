64af75b3c73c4b99f5f572570ae98b7d628d31cc
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-11-04 15:01:02 +0100

    Implémentation de la fonctionalité Deconnexion

--- Frontend/Js/form/checkUserAccess.js
+++ Frontend/Js/form/checkUserAccess.js
@@ -1,53 +1,95 @@
-const accessFormation = document.querySelectorAll('.resume__main__module');
-const accessMsg = document.querySelector('#accessMsg');
+// const accessFormation = document.querySelectorAll('.resume__main__module');
+// const accessMsg = document.querySelector('#accessMsg');
 const token = localStorage.getItem('token');
-const logoutButton = document.querySelector('.deconnexion');
-
-if(!token) {
-     
-     logoutButton.style.display = 'none';
-}
+const profil = document.querySelector('.profil');
 
 
+if (token) {
 
-accessMsg.style.display = 'none';
+     logoutButton.style.display = 'block';
+     connexionButton.style.display = 'none';
+     inscriptionButton.style.display = 'none';
+     profil.style.display = 'block';
 
-accessFormation.forEach(el => {
-     
-     el.addEventListener('click', deniedAccess)
-     
-});
-
-
-
-
-
-function deniedAccess() {
-     
+    //  const id = localStorage.getItem('id');
  
-     
-     
-     if (!token) {
+    //  const url = `http://localhost:3000/api/getuser/${id}`;
+ 
+    //  fetch(url, {
+    //      headers: {
+    //          'Content-Type' : 'Application/json',
+    //          'Accept' : 'Application/json'
+    //      }
+    //  })
+    //  .then( data => {
+    //      data.json()
+    //      .then( res => {
+    //          userNameDisplay.textContent = `Bienvenue ${res.name} 😃`;
+    //      })
+    //  })
+ } else {
+     logoutButton.style.display = 'none';
+     connexionButton.style.display = 'block';
+     inscriptionButton.style.display = 'block';
+     profil.style.display = 'none';
 
-      
-          accessMsg.style.display = 'inline';
-          accessMsg.style.textAlign = 'center';
-          accessMsg.style.fontSize = '1.5rem';
-        accessMsg.style.color = 'red';
-        accessMsg.style.paddingTop = '0px';
-       
-        
-     setTimeout(() => {
-          window.location.replace('../formationHub.html');
-     }, 1600)
-   
-   } else {
+ }
 
-        window.location.replace('../formationRea.html');
 
-   }
-   
+function logout() {
+
+          localStorage.removeItem('token');
+          window.location.replace('/index.html');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// accessMsg.style.display = 'none';
+
+// accessFormation.forEach(el => {
+     
+//      el.addEventListener('click', deniedAccess)
+     
+// });
+
+
+
+// function deniedAccess() {
+     
+//      if (!token) {
+
+//           accessMsg.style.display = 'inline';
+//           accessMsg.style.textAlign = 'center';
+//           accessMsg.style.fontSize = '1.5rem';
+//         accessMsg.style.color = 'red';
+//         accessMsg.style.paddingTop = '0px';
+         
+//      setTimeout(() => {
+//           window.location.replace('../formationHub.html');
+//      }, 1600)
+   
+//    } else {
+
+//         window.location.replace('../formationRea.html');
+
+//    }
+   
+// }
 
 
 
