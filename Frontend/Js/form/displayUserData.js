bd05f33f23ced1f0cf8b290387857d634e83b045
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-11-08 14:27:46 +0100

    Modification de la fonction fetch get sous condition if

--- Frontend/Js/form/displayUserData.js
+++ Frontend/Js/form/displayUserData.js
@@ -1,28 +1,43 @@
 
-const profilInfo = document.querySelector('.profil__informations');
-
-const id = localStorage.getItem('id') || sessionStorage.getItem('token');
+    const id = localStorage.getItem('id') || sessionStorage.getItem('id');
 
 
+    const profilInfo = document.querySelector('.profil__informations');
 
 
-fetch( `http://localhost:3000/api/getuser/${id}`, {
-    method : "get",
-    headers : { "Content-Type" : "Application/json",
-                "Accept" : "Application/json"}
-})
-.then( data => {
-  return data.json(); 
-}).then( (res) => {
-    console.log(res);
+    if(id) {
 
-    for (let info in res) {
+        fetch( `http://localhost:3000/api/getuser/${id}`)
+    .then( data => {
+        console.log(data);
+      return data.json();
+    })
+    .then( (res) => {
+        console.log(res);
+    
+    
+        for ( let info in res) {
+    
+            profilInfo.innerHTML += `<h3>Nom : ${info.name} </h3>
+            <h3>Prénom : ${info.secondName}</h3>
+            <h3>email : ${info.email}</h3>
+            <h3>Tél : ${info.telephone}</h3>
+            <h3>Mot de passe : ${info.password}</h3>
+            <h3>Document type : ${info.documentType}</h3> 
+    `
+    
+        }
+    
+    })
 
-        profilInfo.innerHTML += `<h3>Nom : ${info.name} </h3>
-                            <h3>Prénom : ${info.secondName}</h3>
-                            <h3>email : ${info.email}</h3>
-                            <h3>Tél : ${info.telephone}</h3>
-                            <h3>Mot de passe : ${info.password}</h3>
-                            <h3>Document type : ${info.documentType}</h3> 
-`}
-}).catch(err => console.log(err))
+    } else {
+
+        window.location.replace('../pages/index.html')
+
+    }
+    
+    
+    
+
+
+
