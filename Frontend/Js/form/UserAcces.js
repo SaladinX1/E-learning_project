5982f7e36e019f5a41f970f95947078b8e71ddbf
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-11-09 17:08:16 +0100

    Modification du message alert dans UserAccess.js

--- Frontend/Js/form/UserAcces.js
+++ Frontend/Js/form/UserAcces.js
@@ -1,9 +1,9 @@
-
+// const token = localStorage.getItem('token') || sessionStorage.getItem('token');
 
 window.addEventListener('load', () => {
 
     if(!token) {
-        alert(`Veuillez vous connecter s'il vous plaît, merci`);
+        alert(` | ! | Veuillez vous connecter s'il vous plaît, merci (Accès non Autorisé)`);
         window.location.replace('/index.html');
     }
 
