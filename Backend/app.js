1aa26aca2e07f38b0c46e42e8bbea4e85cafb94c
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-11-09 11:58:26 +0100

    Etablissement du express.json() pour traitement requête json et url

--- Backend/app.js
+++ Backend/app.js
@@ -9,10 +9,10 @@ const auth = require('./Midlewares/auth');
 
  app.use(cors());
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json())
+ app.use(express.json())
+ app.use(express.urlencoded( { extended: true } ));
+
+ 
 
 app.use('/api', userRoutes);
 
