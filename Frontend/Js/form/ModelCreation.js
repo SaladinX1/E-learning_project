29050ad585f56a4cfded6fb2bec344527fdaf965
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-11-23 17:56:22 +0100

    Établissement de addEventListener creaForm pour envoie creation formation en bdd

--- Frontend/Js/form/ModelCreation.js
+++ Frontend/Js/form/ModelCreation.js
@@ -1,40 +1,88 @@
-// const User = require('../../../Backend/Models/User');
+
 
 let logoutButton = document.querySelector('.deconnexion')
-let connexionButton = document.querySelector('.connexion');
-let inscriptionButton = document.querySelector('.inscription');
+ //const token = localStorage.getItem('token');
+ //const id = localStorage.getItem('id');
+const formulaireCreation = document.querySelector('.creaForm');
 
-const name = document.querySelector('#name');
+
 const price = document.querySelector('#price');
-const duration = document.querySelector('#duration');
-const document = document.querySelector('#document');
-const document2 = document.querySelector('#document2');
-const document3 = document.querySelector('#document3');
-const document4 = document.querySelector('#document4');
-const document5 = document.querySelector('#document5');
-const document6 = document.querySelector('#document6');
-const document7 = document.querySelector('#document7');
-const document8 = document.querySelector('#document8');
-const document9 = document.querySelector('#document9');
-const document10 = document.querySelector('#document10');
+
+
+price.addEventListener('change', () => {
+
+    if( document.querySelector('#price').value < 0) {
+        return document.querySelector('#price').textContent = 0;
+    }
+
+})
+
+
+
 
 
 if (token && id) {
 
     logoutButton.style.display = 'block';
-    connexionButton.style.display = 'none';
-    inscriptionButton.style.display = 'none';
     profil.style.display = 'block';
 
 } else{
 
     logoutButton.style.display = 'none';
-    connexionButton.style.display = 'block';
-    inscriptionButton.style.display = 'block';
     profil.style.display = 'none';
 
 }
 
 
 
- fetch('http://localhost:3000/api/createFormation')
+
+ 
+
+
+
+formulaireCreation.addEventListener('submit', (e) => {
+         e.preventDefault();
+
+
+         function createFormation () {
+
+
+
+            const newFormation = {
+                name: document.querySelector('#name').value,
+                price: document.querySelector('#price').value,
+                duration:  document.querySelector('#duration').value,
+                file: document.querySelector('#document').value,
+                file2: document.querySelector('#document2').value,
+                file3: document.querySelector('#document3').value,
+                file4: document.querySelector('#document4').value,
+                file5: document.querySelector('#document5').value,
+                file6: document.querySelector('#document6').value,
+                file7: document.querySelector('#document7').value,
+                file8: document.querySelector('#document8').value,
+                file9: document.querySelector('#document9').value,
+                file10: document.querySelector('#document10').value,
+        
+            }
+        
+        
+        
+        
+            fetch('http://localhost:3000/api/createFormation', {
+            method: 'POST',
+            body: JSON.stringify(newFormation),
+            headers: {
+                'authorization' : `Bearer ${token}`
+            }
+        }).then( res => {
+            alert('Bravo ! La Formation a été crée :)')
+        })
+        .catch(error => console.error(error))
+        
+        
+        
+        }
+
+        
+})
+
