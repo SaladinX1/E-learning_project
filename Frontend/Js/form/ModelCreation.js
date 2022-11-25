0f79e172002a81f2c79fef88335c05a12c11fc1d
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-11-25 17:07:24 +0100

    Tentative changement de selection dynamique ajout fichier + tentative réglement requête getAllFormation affichage

--- Frontend/Js/form/ModelCreation.js
+++ Frontend/Js/form/ModelCreation.js
@@ -1,7 +1,9 @@
 
 const displayFileButton = document.querySelector('.displayButton');
 const file_group = document.querySelector('.file-group');
-let logoutButton = document.querySelector('.deconnexion')
+let logoutButton = document.querySelector('.deconnexion');
+
+const choiceInput = document.querySelector('.choiceInput');
 
 const formulaireCreation = document.querySelector('.creaForm');
 
@@ -65,6 +67,7 @@ function createFormation() {
             }
         }).then( res => {
             alert('Bravo ! La Formation a été crée :)')
+            window.location.reload();
         })
         .catch(error => console.error(error))
 
@@ -79,67 +82,153 @@ function createFormation() {
 
 function displayFileInput() {
     
+    choiceInput.innerHTML = '';
+    
+    choiceInput.style.display = 'block';
+    choiceInput.style.border = '1px solid black';
+    choiceInput.style.margin = '0 10px';
+    choiceInput.style.padding = '10px';
+    choiceInput.style.borderRadius = '10px';
+    choiceInput.innerHTML +=  
+    
+    ` <h3>Veuillez chosir combien de fichier(s) à ajouter :</h3>
+    <label>Vidéo : </label>
+    <input id='videoNumber' type='number'/>
+    <label>Pdf : </label>
+    <input id='pdfNumber' type='number'/> 
+    `;
 
-    if(showFiles == false )  {
+    //let idVideoNb = document.querySelector('   #videoNumber');
+    //let idPdfNb = document.querySelector('#pdfNumber');
 
-            file_group.style.display = 'block';
-        displayFileButton.innerText = '-';
-        showFiles = true;
+  //  choiceInput.append()
 
-        return;
+
+    let nbVideos = document.querySelector('#videoNumber').value;
+    let nbPdfs = document.querySelector('#pdfNumber').value;
+
+    choiceInput.innerHTML += `<button onclick='hideChoice()' ">Annuler</button> <button id="validationFilesAdded" onclick='displayFilesSelected(${nbVideos, nbPdfs})' >Valider</button>`;                        
+
+    // if(showFiles == false )  {
+
+    //         file_group.style.display = 'block';
+    //     displayFileButton.innerText = '-';
+    //     showFiles = true;
+
+    //     return;
 
     
-    } else  if(showFiles  == true) {
+    // } else  if(showFiles  == true) {
 
-        file_group.style.display = 'none';
-        displayFileButton.innerText = '+'
-        showFiles = false;
+    //     file_group.style.display = 'none';
+    //     displayFileButton.innerText = '+'
+    //     showFiles = false;
 
-        return;
+    //     return;
         
+    // }
+
+}
+
+function hideChoice() {
+
+    choiceInput.style.display = 'none';
+
+}
+
+
+document.querySelector('#validationFilesAdded').addEventListener('click', displayFilesSelected);
+
+
+function displayFilesSelected(nbv, nbf) {
+
+    
+
+    if (nbv > 0) {
+
+        let inputVideo = formulaireCreation.innerHTML += ` 
+         <div class="input-group">
+        <label for="video">Vidéo :</label>
+        <input id="video" type="file">
+         </div> 
+         `;
+         
+       let inputVideoCreated =  nbv * inputVideo;
+      
+       let inputVideoDisplayed = formulaireCreation.appendChild(inputVideoCreated);
+
+       return inputVideoDisplayed;
+    } else if (nbf > 0) {
+
+        let inputPdf = formulaireCreation.innerHTML += ` 
+        <div class="input-group">
+        <label for="document">PDF :</label>
+        <input class="doc" name="file"  id="document" type="file" >
+      </div>
+        `;
+
+        let inputPdfCreated =  nbf * inputPdf;
+      
+        let inputpdfDisplayed = formulaireCreation.appendChild(inputPdfCreated);
+ 
+        return inputpdfDisplayed;
+
     }
 
 }
 
 
+
 // Fonctionnalité de récupération des formation depuis la BDD
 
 
-(function getAllFormations() {
+function getAllFormations() {
 
     const token = localStorage.getItem('token');
 
     fetch('http://localhost:3000/api/formations', {
         method: 'GET',
         headers: {
-            'accept' : 'application/json',
-            'content-type' : 'application/json',
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json',
             'authorization' : `Bearer ${token}`
         }
     })
     .then(data => { return data.json(); })
     .then(res => { 
-
-        let containerFormations = document.querySelector('.recoverAllFormation');
-
+console.log(res.body);
         for(let formation in res) {
+
+
             
-            containerFormations.innerHTML += ` <h1> ${formation.name} </h1>
-                                                <p> ${formation.price} </p>
-                                                <p> ${formation.duration} </p>
-                                                <p> ${formation.file} </p>
-                                                <p> ${formation.file2} </p>
-                                                <p> ${formation.file3} </p>
-                                                <p> ${formation.file4} </p>
-                                                <p> ${formation.file5} </p>
-                                                <p> ${formation.file6} </p>
-                                                <p> ${formation.file7} </p>
-                                                <p> ${formation.file8} </p>
-                                                <p> ${formation.file9} </p>
-                                                <p> ${formation.file10} </p>
-                                                
-            ` 
+            
+            
+            //  for(let i; i < formation.length ; i++) {
+                
+                document.querySelector('.recoverAllFormation').innerHTML += `
+                
+                                                <div>
+                                                   <h1> ${formation.name} </h1>
+                                                    <p> ${formation.price} </p>
+                                                    <p> ${formation.duration} </p>
+                                                    <p> ${formation.file} </p>
+                                                    <p> ${formation.file2} </p>
+                                                    <p> ${formation.file3} </p>
+                                                    <p> ${formation.file4} </p>
+                                                    <p> ${formation.file5} </p>
+                                                    <p> ${formation.file6}</p>
+                                                    <p> ${formation.file7}</p>
+                                                    <p> ${formation.file8}</p>
+                                                    <p> ${formation.file9}</p>
+                                                    <p> ${formation.file10} </p>
+                                                 </div>
+                                                    
+                ` 
+         // }
         }
+        
  }).catch(err => console.log(err));
 
-})();
+};
+
+getAllFormations();
