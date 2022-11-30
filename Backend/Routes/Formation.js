dc984179932daa5d67f39e08a1844b42b978e273
Saladin_Rising <hopefulhorizon01@gmail.com>
2022-11-30 17:15:51 +0100

    Etablissement des paramètre id Dynamique sur les routes formations

--- Backend/Routes/Formation.js
+++ Backend/Routes/Formation.js
@@ -7,9 +7,9 @@ const auth = require('../Midlewares/auth')
 
 
 router.post('/createFormation', auth,  formationCtrl.create);
-router.delete('/deleteFormation', auth, formationCtrl.delete);
-router.put('/putFormation',auth,  formationCtrl.put);
+router.delete('/deleteFormation/:id', auth, formationCtrl.delete);
+router.put('/putFormation/id',auth,  formationCtrl.put);
 router.get('/formations',auth,  formationCtrl.getAll);
-router.get('/formation',auth,  formationCtrl.getOne);
+router.get('/formation/:id',auth,  formationCtrl.getOne);
 
 module.exports = router;
