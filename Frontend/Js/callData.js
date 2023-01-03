936a17e4638626f6d0400226d8c7a448bbb2ea64
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-01-03 17:08:46 +0100

    Ajout de la localisation dynamique pour injection data dans callData.js

--- Frontend/Js/callData.js
+++ Frontend/Js/callData.js
@@ -1,14 +1,57 @@
 
-
 const exploitants = document.querySelector('#exploitants');
 const enseignants = document.querySelector('#enseignants');
 const formationX = document.querySelector('#formationX');
+const timer = document.querySelector('.timer');
+ let time2countDown = localStorage.getItem('timeFormation');
 
 const data = localStorage.getItem('formationData');
 
+if ( document.URL.includes("formation3.html") ) {
 
- exploitants.innerHTML = data;
+    
+    const time = parseInt(time2countDown);
+    console.log(typeof(time)); 
 
- enseignants.innerHTML = data;
+  setTimeout( () => {
 
- formationX.innerHTML = data;
+    time - 1;
+    timer.innerText = time;
+
+ }, 1000);
+    timer.innerHTML = timer;
+    
+    formationX.innerHTML = data;
+
+} else if( document.URL.includes("formationEnseignants.html")) {
+
+    const time = parseInt(time2countDown);
+
+    setTimeout( () => {
+  
+      time - 1;
+      timer.innerText = time;
+  
+   }, 1000)
+      timer.innerHTML = timer;
+
+    enseignants.innerHTML = data;
+
+} else if ( document.URL.includes("formationExploitants.html")) {
+
+    const time = parseInt(time2countDown);
+
+    setTimeout( () => {
+  
+      time - 1;
+      timer.innerText = time;
+  
+   }, 1000)
+      timer.innerHTML = timer;
+
+    exploitants.innerHTML = data;
+}
+
+
+
+
