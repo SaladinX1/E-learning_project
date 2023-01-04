84cddf187ef67ad798c654504407fa8ec61ba629
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-01-04 17:04:17 +0100

    Première tentative établissement timer

--- Frontend/Js/callData.js
+++ Frontend/Js/callData.js
@@ -7,51 +7,156 @@ const timer = document.querySelector('.timer');
 
 const data = localStorage.getItem('formationData');
 
+
+let timerId;
+
+
 if ( document.URL.includes("formation3.html") ) {
+  
+  let initialTime = parseInt(time2countDown);
+  let Minutes = 59;
+  let time2Seconds = 59;
+  
+  initialTime--;
+  function formatedTime(timeHour, timeMinutes, timeSeconds) {
+    return `${timeHour}:${timeMinutes}:${timeSeconds % 60 < 10 ? `0${timeSeconds % 60}`: timeSeconds % 60}`;
+  };
+  
 
+  timerId = setInterval( (() => {
+
+
+      if(initialTime > 0 && Minutes > 0 && time2Seconds > 0) {
+       // initialTime--;
+       time2Seconds--;
+        timer.textContent = formatedTime(initialTime,Minutes, time2Seconds);
+        return time2Seconds;
+
+      }else if( time2Seconds == 0) {
     
-    const time = parseInt(time2countDown);
-    console.log(typeof(time)); 
+        Minutes--;
+        time2Seconds = 59
+       timer.textContent = formatedTime(initialTime, Minutes, time2Seconds);
+    return Minutes, time2Seconds;
 
-  setTimeout( () => {
 
-    time - 1;
-    timer.innerText = time;
+      }else if(initialTime > 0 && Minutes == 0 && time2Seconds == 0) {
+        initialTime--;
+        Minutes = 59;
+        time2Seconds = 59;
+        timer.textContent = formatedTime(initialTime, Minutes, time2Seconds);
+        return initialTime ,Minutes, time2Seconds;
 
- }, 1000);
-    timer.innerHTML = timer;
+
+      } else if( initialTime == 0 && time2Seconds == 0) {
+        timer.textContent = ' Vous avez passé le temps requis pour tester vos connaissances avec un quizz, bravo !';
+        clearInterval(timerId);
+      }
+    
+
+  }), 1000);
     
     formationX.innerHTML = data;
 
+
+
 } else if( document.URL.includes("formationEnseignants.html")) {
 
-    const time = parseInt(time2countDown);
 
-    setTimeout( () => {
+  let initialTime = parseInt(time2countDown);
+  let Minutes = 59;
+  let time2Seconds = 59;
   
-      time - 1;
-      timer.innerText = time;
+  initialTime--;
+  function formatedTime(timeHour, timeMinutes, timeSeconds) {
+    return `${timeHour}:${timeMinutes}:${timeSeconds % 60 < 10 ? `0${timeSeconds % 60}`: timeSeconds % 60}`;
+  };
   
-   }, 1000)
-      timer.innerHTML = timer;
+
+  timerId = setInterval( (() => {
+
+
+      if(initialTime > 0 && Minutes > 0 && time2Seconds > 0) {
+       // initialTime--;
+       time2Seconds--;
+        timer.textContent = formatedTime(initialTime,Minutes, time2Seconds);
+        return time2Seconds;
+
+      }else if( time2Seconds == 0) {
+    
+        Minutes--;
+        time2Seconds = 59
+       timer.textContent = formatedTime(initialTime, Minutes, time2Seconds);
+    return Minutes, time2Seconds;
+
+
+      }else if(initialTime > 0 && Minutes == 0 && time2Seconds == 0) {
+        initialTime--;
+        Minutes = 59;
+        time2Seconds = 59;
+        timer.textContent = formatedTime(initialTime, Minutes, time2Seconds);
+        return initialTime ,Minutes, time2Seconds;
+
+
+      } else if( initialTime == 0 && time2Seconds == 0) {
+        timer.textContent = ' Vous avez passé le temps requis pour tester vos connaissances avec un quizz, bravo !';
+        clearInterval(timerId);
+      }
+    
+
+  }), 1000);
+
 
     enseignants.innerHTML = data;
 
+
+
 } else if ( document.URL.includes("formationExploitants.html")) {
 
-    const time = parseInt(time2countDown);
 
-    setTimeout( () => {
+
+  let initialTime = parseInt(time2countDown);
+  let Minutes = 59;
+  let time2Seconds = 59;
   
-      time - 1;
-      timer.innerText = time;
+  initialTime--;
+  function formatedTime(timeHour, timeMinutes, timeSeconds) {
+    return `${timeHour}:${timeMinutes}:${timeSeconds % 60 < 10 ? `0${timeSeconds % 60}`: timeSeconds % 60}`;
+  };
   
-   }, 1000)
-      timer.innerHTML = timer;
+
+  timerId = setInterval( (() => {
+
+
+      if(initialTime > 0 && Minutes > 0 && time2Seconds > 0) {
+       // initialTime--;
+       time2Seconds--;
+        timer.textContent = formatedTime(initialTime,Minutes, time2Seconds);
+        return time2Seconds;
+
+      }else if( time2Seconds == 0) {
+    
+        Minutes--;
+        time2Seconds = 59
+       timer.textContent = formatedTime(initialTime, Minutes, time2Seconds);
+    return Minutes, time2Seconds;
+
+
+      }else if(initialTime > 0 && Minutes == 0 && time2Seconds == 0) {
+        initialTime--;
+        Minutes = 59;
+        time2Seconds = 59;
+        timer.textContent = formatedTime(initialTime, Minutes, time2Seconds);
+        return initialTime ,Minutes, time2Seconds;
+
+
+      } else if( initialTime == 0 && time2Seconds == 0) {
+        timer.textContent = ' Vous avez passé le temps requis pour tester vos connaissances avec un quizz, bravo !';
+        clearInterval(timerId);
+      }
+    
+
+  }), 1000);
 
     exploitants.innerHTML = data;
 }
-
-
-
-
