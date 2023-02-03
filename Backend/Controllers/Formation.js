c1ec851364c7b99eb9e2b54dd228d2bffc3853a1
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-02-03 17:55:19 +0100

    Refactorisation des données liées au traitement des videos et pdf en une seul sources documents

--- Backend/Controllers/Formation.js
+++ Backend/Controllers/Formation.js
@@ -4,45 +4,41 @@ const { json, where } = require('sequelize');
 const Storage = require('node-storage');
 const app = require('../app');
 let sequelize = require('../Database/db.script');
- const pdfParse = require("pdf-parse");
 
 
-let videoSetId =  Math.random().toString(36).slice(2); 
- let filesSetId =  Math.random().toString(36).slice(2); 
-let store = new Storage(`../Frontend/videos/${videoSetId}`);
-let store2 = new Storage(`../Frontend/textFiles/${filesSetId}`);
-let videoStore = [];
-let filesStore = [];
+ let docsSetId =  Math.random().toString(36).slice(2); 
+
+let storeDocs = new Storage(`../Frontend/docs/${docsSetId}`);
+
+//let docsStore = [];
 
 exports.create =  (req, res) => {
 
-   // console.log(req.file);
+
+   // console.log('doc Set Id:', docsSetId);
+  
+   const allDocs = docsSetId;
+
+   const {
+       nameFormation,
+       priceFormation,
+       durationFormation,
+       role
+    } = req.body;
     
-   // console.log(req.body.picture);
-
-    const videos = videoSetId;
-    const pdfs = filesSetId;
- //  console.log(req.file.filename); 
-   // const { picture } =  `${req.protocol}://${req.get('host')}/Frontend/images/${req.file.filename}`;
-    
-
-    const {
-         nameFormation,
-        priceFormation,
-        picture,
-        durationFormation,
-        role
-        } = req.body;
-
+    // console.log( req.user.id,
+    //    nameFormation,
+    //    priceFormation,
+    //    durationFormation,
+    //    docs,
+    //    role);
         try {
                     const formationNew = new Formation ({
                         UserId: req.user.id,
                         nameFormation,
                         priceFormation,
                         durationFormation,
-                        picture,
-                        videos,
-                        pdfs,
+                        allDocs,
                         role
                     });
                     formationNew.save()
@@ -117,115 +113,33 @@ exports.delete = (req, res) => {
 }
 
 
-exports.storeVideo = (req,res) => {
-    
-   // console.log(req.body.videos);
+exports.storeDocs = (req,res) => {
 
-   // console.log(videoSetId);
+    console.log(docsSetId);
 
     try {
-        const videosArr = req.body.videos;
+        const docsArr = req.body.documents;
      
-            for ( let sample of videosArr ) {
-                
-                
-                for (let i = 0; i < videosArr.length; i++ ) {
-                    
-                    store.put(`${sample}`);
-                    
+            for ( let sample of docsArr ) {
+
+            
+                for (let i = 0; i < docsArr.length; i++ ) {    
+                    storeDocs.put(`${sample}`);
                 }
-                
             }
 
-            if (videosArr == ' ') {
-                videoStore.push(null);
-            } else {
-                
-                videoStore.push(videoSetId);
-
+            if (docsArr == ' ') {
+             return;
             }
 
-   console.log(videoStore);
       
     } catch (err) {
         console.log( 'Rendu erreur = ' + err);
        // console.log(e.msg);
     }
-
- }
-
+}
 
 
-
- exports.storeFiles = (req, res) => { 
-
-  //  console.log(req.body.files);
-
-    try {
-        const filesArr = req.body.files;
-     
-            for ( let sample of filesArr ) {
-                
-                
-                for (let i = 0; i < filesArr.length; i++ ) {
-                    
-                    store2.put(`${sample}`);
-                    
-                }
-                
-            }
-
-            if (filesArr == ' ') {
-                filesStore.push(null);
-            } else {
-                console.log(filesSetId);
-                filesStore.push(filesSetId);
-
-            }
-
-  // console.log(filesStore);
-      
-    } catch (err) {
-        console.log( 'Rendu erreur = ' + err);
-       // console.log(e.msg);
-    }
- }
-
- 
-//  let textTab = [];
-//  exports.pdfsTreatment = (req,res, next) => {
-
-//     const pdfs = req.body.files;
-//     let pathPdfs = [];
-//    // console.log(pdfs);
-//     for (let pdf of pdfs) {
-
-//        let formatPath = pdf.replace('C:\\fakepath\\', '..\\Frontend\\pdfsData\\');
-//        pathPdfs.push(formatPath);
-//     //console.log(document.URL);
-//        // console.log(formatPath);
-
-//     }
-    
-//    // console.log(pathPdfs);
-
-//     let dataTextTab = [];
-//         for (let path of pathPdfs) {
-//             let pdfFile = fs.readFileSync(path);
-
-//             // obtenir l'information  
-//             pdfParse(pdfFile).then(function (data) {
-
-//                 let dataText = data.text;
-
-//                 console.log(dataText);
-//                     return dataText;
-//                 //    console.log(dataTextTab);
-                
-//                  })
-//         }
-
-//  }
 
 //  exports.deleteVideos = (req, res, next) => {
 
@@ -265,39 +179,3 @@ exports.storeVideo = (req,res) => {
 //     // store.remove(idSet2Delete);
 //   }
 
-//   exports.getVideos = (req, res, next) => {
-
-//     console.log(req.params.id);
-
-//     Formation.findOne({
-//             where: {
-//             id : req.params.id
-//         }
-//     })
-//     .then(data => {
-
-//         console.log(data);
-//         res.status(200).json(data.videos);
-
-//         // if(!data) {
-//         //     res.status(200).json({message: 'Aucune vidéo disponible'})
-//         // } else {
-//         //     videoStore.filter(idSet => {
-//         //         if(idSet === data) {
-                    
-//         //             fetch(`C:/Users/Utilisateur/Desktop/folder clone/E-learning_project/Frontend/videos/${data}`)
-//         //             .then(videoSet => {
-//         //                 res.status(200).json({message: 'vidéos récupérés', data: videoSet})
-//         //             })
-//         //             .catch(res.status(500).json({message: 'Erreur local path...'}))
-//         //         }
-//         //      })
-     
-             
-//         // }
-        
-//     })
-//     .catch(res.status(500).json({message: 'Erreur Serveur ...'}))
-
-
-//   }
