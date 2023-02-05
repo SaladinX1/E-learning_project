34eaa03663acc6800abb0b046143e0ef42a164c4
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-02-05 13:42:45 +0100

    Transfer suppression user code dans logManagement.js

--- Frontend/Js/logManagement.js
+++ Frontend/Js/logManagement.js
@@ -145,6 +145,8 @@ const userNameDisplay = document.querySelector('.userDisplay');
   } 
  })
 
+
+ // GESTION DECONNEXION UTILISATEUR
  
  function logout() {
      if(confirm('Voulez-vous vraiment vous déconnecter ?')) {
@@ -153,3 +155,40 @@ const userNameDisplay = document.querySelector('.userDisplay');
            window.location.replace('/index.html');
      }
  }
+
+
+ // GESTION SUPPRESSION COMPTE UTILISATEUR 
+
+
+ 
+const deleteUserButton = document.querySelector('.suppression');
+
+deleteUserButton.addEventListener('click', deleteAccount);
+
+
+function deleteAccount() {
+
+    if(confirm('Êtes-vous sûr de vouloir supprimer votre compte ? \b\r \b\r Cette action est définitive.')) {
+         
+
+        fetch( `http://localhost:3000/api/destroyuser/${id}`, 
+        {method : 'delete',
+        headers :  {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json',
+            'authorization' : `Bearer ${tokenBis}`
+        }})
+        .then( res => {
+        alert('Votre compte a bien été supprimé ! ')
+        localStorage.removeItem('id'),
+            localStorage.removeItem('token'),
+            sessionStorage.removeItem('token')
+        window.location.replace('../../index.html');
+        })
+        .catch(err =>  console.log(err))
+
+    }
+  
+
+}
+
