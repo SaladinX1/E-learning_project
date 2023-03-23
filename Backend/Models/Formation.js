775e50085c7098f5fd8a987c84856d5c48442a32
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-03-23 11:36:50 +0100

    Modification du modèle de données formation

--- Backend/Models/Formation.js
+++ Backend/Models/Formation.js
@@ -5,44 +5,57 @@ const User = require('./User');
 
 
 const Formation = sequelize.define('Formation', {
-
     id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
     },
     nameFormation: {
-        type: Sequelize.STRING,
-        allowNull: true,
-        defaultValue: false
+      type: Sequelize.STRING(255),
+      allowNull: false,
+      defaultValue: ''
     },
     priceFormation: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        defaultValue: false
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
     },
     namesModules: {
-        type: Sequelize.DataTypes.ARRAY(Sequelize.DataTypes.STRING),
-        allowNull: false,
-        defaultValue: false
+      type: Sequelize.JSON,
+      allowNull: false,
+      defaultValue: '[]'
     },
     durationFormation: {
-       type: Sequelize.STRING,
-       allowNull: false,
-        defaultValue: false
+      type: Sequelize.STRING(255),
+      allowNull: false,
+      defaultValue: ''
     },
     docsFormationCodes: {
-        type: Sequelize.DataTypes.ARRAY(Sequelize.DataTypes.STRING),
-        allowNull: false,
-        defaultValue: false
+      type: Sequelize.JSON,
+      allowNull: false,
+      defaultValue: '[]'
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false
     }
-})
-
+  }, {
+    tableName: 'Formations',
+    engine: 'InnoDB'
+  });
+  
+  Formation.belongsTo(User, { foreignKey: 'UserId', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
+  
+  //sequelize.sync();
 
 
 User.hasMany(Formation);
-Formation.belongsTo(User);
+//Formation.belongsTo(User);
 
 Formation.hasMany(Module);
 Module.belongsTo(Formation);
