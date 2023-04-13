e4aa65ebdae242133bc3858a00a263fdd974ede6
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-04-13 11:19:15 +0200

    Établissement des controllers FormationsAchetes + model ProduitsAchetés

--- Backend/Controllers/ProduitsAchetes.js
+++ Backend/Controllers/ProduitsAchetes.js
@@ -1,12 +1,60 @@
-const PA = require('../Models/ProduitsAchetes');
-
+const Pa = require('../Models/ProduitsAchetes')
+const Formation = require('../Models/Formation');
+const User = require('../Models/User');
 
 exports.postPA = (req, res) => {
+    
+    const userId = req.params.id;
+
+    const {formationId, dateAchat, price, name } = req.body;
+
+
+// Creation entrée table de liaison
+
+const FormationAchete = new Pa({
+
+    nom: name,
+    prix: price,
+    userId: userId,
+    referenceId: formationId,  
+    date_achat: dateAchat,
+
+}) 
+FormationAchete.save()
+.then(res.status(201).json({
+    message: ' La référence du produit a bien été enregistrée 😃'
+})
+)
+.catch(error => console.log(error));
 
 }
 
 
 exports.getPA = (req, res) => {
 
+    const userId = req.params.id;
+
+    //recherche user 
+
+    User.findOne({
+        where: {
+            id: userId
+        },
+        include: [{
+            model: Formation,
+            through: {
+                model: Pa,
+                attributes: ['date_achat']
+            }
+        }]
+    })
+    .then(user => {
+        // renvoie données format Json
+        res.json(user.formations);
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(500).send('Une erreur est survenue');
+    })
     
 }
--- Backend/Models/ProduitsAchetes.js
+++ Backend/Models/ProduitsAchetes.js
@@ -6,9 +6,16 @@ const Formation = require('./Formation');
 
 
 const ProduitsAchetes = sequelize.define('produits_achetes', {
+    nom: Sequelize.STRING,
+    prix: Sequelize.FLOAT,
+    userId: Sequelize.INTEGER,
+    referenceId: Sequelize.STRING,  
     date_achat: Sequelize.DATE,
   });
 
 
   User.belongsToMany(Formation, { through: ProduitsAchetes });
   Formation.belongsToMany(User, { through: ProduitsAchetes });
+
+
+  module.exports = ProduitsAchetes;
