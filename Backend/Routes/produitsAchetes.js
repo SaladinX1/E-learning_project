04089c67e919750b12d634391520153eccbfae05
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-04-17 00:22:49 +0200

    Retrait du middleware multer dans la route postPa

--- Backend/Routes/produitsAchetes.js
+++ Backend/Routes/produitsAchetes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const PACtrl = require('../Controllers/ProduitsAchetes');
 const auth = require('../Midlewares/auth');
-const multer = require('../Midlewares/multer');
 
-router.post('/getuser/:id/formation', auth, multer, PACtrl.postPA);
+
+router.post('/getuser/:id/formation', auth, PACtrl.postPA);
 router.get('/getuser/:id/formations',auth, PACtrl.getPA);
 
 module.exports = router;
