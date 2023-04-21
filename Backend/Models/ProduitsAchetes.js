df8184b34ab263ca4c480ae72ae7e11ae0a8dd79
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-04-21 17:09:43 +0200

    Ajout des champs modificateur progression User dans modele PA

--- Backend/Models/ProduitsAchetes.js
+++ Backend/Models/ProduitsAchetes.js
@@ -19,18 +19,37 @@ const ProduitsAchetes = sequelize.define('produits_achetes', {
 
       type: Sequelize.DATE,
       allowNull: false,
-
+  },
+      FormationId: {
+          
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          
+        },
+        UserId: {
+            type: Sequelize.INTEGER,
+            //allowNull: false,
+        },
+    notation : {
+       type: Sequelize.DataTypes.INTEGER,
+       allowNull: true,
+       defaultValue: 0
     },
-FormationId: {
-
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    
-},
-UserId: {
-    type: Sequelize.INTEGER,
-    //allowNull: false,
-}
+    progressTime : {
+       type: Sequelize.DataTypes.INTEGER,
+       allowNull: true,
+       defaultValue: 0
+    },
+    idModuleProgress : {
+       type: Sequelize.DataTypes.INTEGER,
+       allowNull: true,
+       defaultValue: 0
+    },
+    barProgress : {
+       type: Sequelize.DataTypes.INTEGER,
+       allowNull: true,
+       defaultValue: 0
+    },
 })
 
 
