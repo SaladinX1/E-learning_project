e794572c3116c5d77e7964dd7194441e96da6f24
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-04-21 17:07:51 +0200

    Établissement de la route patch pour Modification info User

--- Backend/Routes/User.js
+++ Backend/Routes/User.js
@@ -8,7 +8,7 @@ const auth = require('../Midlewares/auth')
 router.post('/register', userCtrl.register);
 router.post('/login', userCtrl.login);
 router.delete('/destroyuser/:id',auth, userCtrl.deleteUser);
-router.put('/updateuser/:id',auth, userCtrl.putUser)
+router.patch('/updateuser/:id',auth, userCtrl.putUser)
 router.put('/updateaccess/:id', auth, userCtrl.putAccess);
 router.get('/getuser/:id',auth, userCtrl.getUser);
 
