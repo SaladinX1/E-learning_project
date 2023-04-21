a22e9605230148ecd7f8c20d3b5738ab0fdffc59
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-04-21 17:07:23 +0200

    Établissement de la route patch pour PA User

--- Backend/Routes/produitsAchetes.js
+++ Backend/Routes/produitsAchetes.js
@@ -7,6 +7,8 @@ const auth = require('../Midlewares/auth');
 
 router.post('/getuser/:id/formation', auth, PACtrl.postPA);
 router.get('/getuser/:id/formations',auth, PACtrl.getPA);
+router.patch('/getuser/:id/formationprogress',auth, PACtrl.putPa);
 router.delete('/removerelation/:id',auth, PACtrl.removeRelation,);
+
 
 module.exports = router;
