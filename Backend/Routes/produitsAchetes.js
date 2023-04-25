7e34832e3e17188c2baae6a6a42bf186eafc5245
Saladin_Rising <hopefulhorizon01@gmail.com>
2023-04-25 17:20:43 +0200

    changement du verbe http pour la route putPa en PUT

--- Backend/Routes/produitsAchetes.js
+++ Backend/Routes/produitsAchetes.js
@@ -7,7 +7,7 @@ const auth = require('../Midlewares/auth');
 
 router.post('/getuser/:id/formation', auth, PACtrl.postPA);
 router.get('/getuser/:id/formations',auth, PACtrl.getPA);
-router.patch('/getuser/:id/formationprogress',auth, PACtrl.putPa);
+router.put('/getuser/:id/formationprogress',auth, PACtrl.putPa);
 router.delete('/removerelation/:id',auth, PACtrl.removeRelation,);
 
 
